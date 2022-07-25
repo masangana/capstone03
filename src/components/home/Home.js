c11ac97cbe75278cffc83a45be2c288a0fbfde50
masangana <alexmasangana@gmail.com>
2022-07-26 01:10:00 +0300

    Add a filtre by continent

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useRef  } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCountries } from '../../redux/countries/countries';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faGear, faMicrophone } from '@fortawesome/free-solid-svg-icons';
 import formatNumber from '../utils/formatNumber';
+import Select from 'react-select';
 import './Home.css';
 import map from '../../assets/afric.png';
 import Grid from './Grid';
 
-const Home = () => {
-  const continent = 'Africa';
+const optionSelect = [
+  { value: 'Africa', label: 'Africa' },
+  { value: 'Asia', label: 'Asia' },
+  { value: 'Europe', label: 'Europe' },
+  { value: 'North America', label: 'North America' },
+  { value: 'Oceania', label: 'Oceania' },
+  { value: 'South America', label: 'South America' },
+];
+let continent = 'Africa';
 
+const Home = () => {
+  const [selected, setSelected] = useState(optionSelect[0].value);
+  
+  const handleChange = event => {
+    console.log('change2 '+event.target.value);
+    setSelected(event.target.value);
+    continent = event.target.value;
+    dispatch(fetchCountries(continent));
+  };
+
+  console.log('change '+continent);
   const dispatch = useDispatch();
   const { items, totalConfirmed, loading } = useSelector((state) => ({
     ...state.countries,
@@ -27,12 +46,32 @@ const Home = () => {
     return null;
   }
 
+  function customTheme(theme) {
+    return {
+      ...theme,
+      colors: {
+        ...theme.colors,
+        primary: 'red',
+        primary25: 'green',
+      },
+    }
+  }
+
   return (
     <section>
       <header className="App-header">
         <FontAwesomeIcon icon={faArrowLeft} />
-        <h4>2021</h4>
+        <h4>2022</h4>
         <h5 className="App-header-title">most views</h5>
+       
+        <select value={selected} onChange={handleChange}>
+          {optionSelect.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.text}
+            </option>
+          ))}
+        </select>
+
         <FontAwesomeIcon icon={faMicrophone} />
         <div className="pl-5">
           <FontAwesomeIcon icon={faGear} />
