fbc057ecc9b78bd4470e3cacb9a59853c58850da
masangana <alexmasangana@gmail.com>
2022-07-26 12:55:46 +0300

    Add information in details view

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -4,7 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import { fetchCountry } from '../../redux/countries/countries';
 import formatNumber from '../utils/formatNumber';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faGear, faMicrophone } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faCity, faGear, faMicrophone } from '@fortawesome/free-solid-svg-icons';
 import image from '../../assets/virus.svg';
 import './Details.css';
 
@@ -26,52 +26,120 @@ const Details = () => {
 
   const { All } = country;
   const list = Object.entries(country).slice(1);
+  console.log(Object.keys(country).length);
 
-  return (
-    <section>
-      <header className="App-header">
-        <Link to="/">
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </Link>
-        <h5 className="App-header-title">view Detailed</h5>
-        <FontAwesomeIcon icon={faMicrophone} />
-        <div className="pl-5">
-          <FontAwesomeIcon icon={faGear} />
+  if (Object.keys(country).length > 1) {
+        return (
+      <section>
+        <header className="App-header">
+          <Link to="/">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </Link>
+          <h5 className="App-header-title">view Detailed</h5>
+          <FontAwesomeIcon icon={faMicrophone} />
+          <div className="pl-5">
+            <FontAwesomeIcon icon={faGear} />
+          </div>
+        </header>
+        <div className="Details-banner">
+          <div className="Details-banner-left">
+            <img src={image} alt="map view" className="App-map" />
+          </div>
+          <div className="Details-banner-right">
+            <h1 className="App-title">{All.country}</h1>
+            <p className="App-subtitle">
+              {`${formatNumber(
+                All.confirmed,
+              )} cases`}
+            </p>
+          </div>
         </div>
-      </header>
-      <div className="Details-banner">
-        <div className="Details-banner-left">
-          <img src={image} alt="map view" className="App-map" />
-        </div>
-        <div className="Details-banner-right">
-          <h1 className="App-title">{All.country}</h1>
-          <p className="App-subtitle">
-            {`${formatNumber(
-              All.confirmed,
-            )} cases`}
-          </p>
-        </div>
-      </div>
-      <section className="Home-stats">
-        <h5 className="App-section-title">DATA in CITY - 2022</h5>
-        <ul>
-          {list.map(([name, { confirmed }]) => (
-            <li key={name} className="Details-item">
-              <h6 className="Details-item-title">{name}</h6>
-              <div className="Details-item-right">
-                <p className="App-subtitle">
-                  {formatNumber(confirmed)}
-                  {' '}
-                  cases
-                </p>
-                <FontAwesomeIcon icon={faArrowLeft} />
-              </div>
-            </li>
-          ))}
-        </ul>
+        <section className="Home-stats">
+          <h5 className="App-section-title">DATA in CITY - 2022</h5>
+          
+          <ul>
+            {list.map(([name, { confirmed }]) => (
+              <li key={name} className="Details-item">
+                <h6 className="Details-item-title">{name}</h6>
+                <div className="Details-item-right">
+                  <p className="App-subtitle">
+                    {formatNumber(confirmed)}
+                    {' '}
+                    cases
+                  </p>
+                  <FontAwesomeIcon icon={faCity} />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
       </section>
-    </section>
-  );
+    );
+  } else if (Object.keys(country).length === 1) {
+    return(
+      <section>
+        <header className="App-header">
+          <Link to="/">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </Link>
+          <h5 className="App-header-title">view Detailed</h5>
+          <FontAwesomeIcon icon={faMicrophone} />
+          <div className="pl-5">
+            <FontAwesomeIcon icon={faGear} />
+          </div>
+        </header>
+        <div className="Details-banner">
+          <div className="Details-banner-left">
+            <img src={image} alt="map view" className="App-map" />
+          </div>
+          <div className="Details-banner-right">
+            <h1 className="App-title">{All.country}</h1>
+            <p className="App-subtitle">
+              {`${formatNumber(
+                All.confirmed,
+              )} cases`}
+            </p>
+          </div>
+        </div>
+        <section className="Home-stats">
+          <h5 className="App-section-title">Country's Data - 2022</h5>
+          
+          <h6 className="App-section-title"> 
+            Confirmed cases : 
+            {` `+`${formatNumber(
+                All.confirmed,
+              )} cases`}
+          </h6>
+          <h6 className="App-section-title">
+            Recovered cases :
+            {` `+`${formatNumber(
+                All.recovered,
+              )} cases`}
+          </h6>
+          <h6 className="App-section-title">
+            Deaths :
+            {` `+`${formatNumber(
+                All.deaths,
+              )} `}
+          </h6>
+          <h6 className="App-section-title">
+            Total Population :
+            {` `+`${formatNumber(
+                All.population,
+              )} peoples`}
+          </h6>
+          <h6 className="App-section-title">
+            Last Update :
+            {` `+`${formatNumber(
+                All.updated,
+              )}`}
+          </h6>
+        </section>
+      </section>
+    )
+  }
+
+
 };
 
 export default Details;
